e221af6f80a9e0105244a15ad42298426bf4ff3f
Ed McCormic <edmccormic@gmail.com>
2015-05-11 17:06:36 -0600

    Initial Commit

---
+++ Hypnosister/GMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GMAppDelegate.h
+//  Hypnosister
+//
+//  Created by ED on 5/11/15.
+//  Copyright (c) 2015 SwiftBeard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

4fe2da3c4f1be1648a55e7274a23d913d2768173
Ed McCormic <edmccormic@gmail.com>
2015-05-11 17:45:37 -0600

    added cool color effects to circle

--- GMHypnosisterView.m
+++ GMHypnosisterView.m
@@ -8,13 +8,21 @@
 
 #import "GMHypnosisterView.h"
 
+@interface GMHypnosisterView ()
+
+@property (strong, nonatomic) UIColor *circleColor;
+
+@end
+
 @implementation GMHypnosisterView
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
+        // All GMHypnosisterViews start with a clear background color
+        self.backgroundColor = [UIColor clearColor];
+        self.circleColor = [UIColor lightGrayColor];
     }
     return self;
 }
@@ -29,24 +37,56 @@
     center.y = bounds.origin.y + bounds.size.height / 2.0;
     
     // The circle will be the largest that will fit in the view
-    float radius = MIN(bounds.size.width, bounds.size.height) / 2.0;
+    //float radius = MIN(bounds.size.width, bounds.size.height) / 2.0;
+    
+    // The Largest circle will circumscribe the view
+    float maxRadius = hypot(bounds.size.width, bounds.size.height) / 2.0;
     
     UIBezierPath *path = [[UIBezierPath alloc] init];
     
     // Add an arc to the path at center, with radius of radius,
     // from 0 to 2*PI radians (a circle)
-    [path addArcWithCenter:center radius:radius startAngle:0.0 endAngle:M_PI * 2.0 clockwise:YES];
+    //[path addArcWithCenter:center radius:radius startAngle:0.0 endAngle:M_PI * 2.0 clockwise:YES];
     
+    
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+        
+        [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
+        
+        [path addArcWithCenter:center radius:currentRadius startAngle:0.0 endAngle:M_PI * 2.0 clockwise:YES];
+    }
     
     // Configure line width to 10 points
     path.lineWidth = 10;
     
     
     // Configure the drawing color to light gray
-    [[UIColor lightGrayColor] setStroke];
+    //[[UIColor lightGrayColor] setStroke];
+    [self.circleColor setStroke];
     
     // Drwa the line!
     [path stroke];
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    
+    NSLog(@"%@ was touched", self);
+    
+    // Get 3 random numbers between 0 and 1
+    float red = (arc4random() %100) / 100.0;
+    float green = (arc4random() %100) / 100.0;
+    float blue = (arc4random() % 100) / 100.0;
+    
+    UIColor *randomColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+    
+    self.circleColor = randomColor;
+}
+
+-(void)setCircleColor:(UIColor *)circleColor
+{
+    _circleColor = circleColor;
+    [self setNeedsDisplay];
 }
 
 /*
